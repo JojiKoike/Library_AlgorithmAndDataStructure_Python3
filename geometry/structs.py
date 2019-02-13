a3b382445be847e210a52c17b8af27e000765e8c
JojiKoike <joji.koike@gmail.com>
2019-02-13 12:57:32 +0900

    Add Calculation Method(abs, norm, <, ==)

--- geometry/structs.py
+++ geometry/structs.py
@@ -2,6 +2,8 @@
 Data Structure Definitions Module
 """
 from dataclasses import dataclass
+import math
+from .common import EPS
 
 
 @dataclass
@@ -11,6 +13,37 @@ class Point:
     """
     x: float
     y: float
+
+    def abs(self) -> float:
+        """
+        Calculate Length
+        :return: Distance From Origin(0, 0)
+        """
+        return math.sqrt(math.pow(self.x, 2.0) + math.pow(self.y, 2.0))
+
+    def norm(self) -> float:
+        """
+        Calculate Norm
+        :return: Norm
+        """
+        return math.pow(self.x, 2.0) + math.pow(self.y, 2.0)
+
+    def __lt__(self, other) -> bool:
+        """
+        Less Than Rule
+        :param other: Other Point
+        :return: Less Than Compare Result
+        """
+        return self.x < other.x if self.x != other.x else self.y < other.y
+
+    def __eq__(self, other) -> bool:
+        """
+        Equality Definition
+        :param other: Other Point
+        :return: Equality Calculation Result
+        """
+        return math.fabs(self.x - other.x) < EPS and math.fabs(self.y - other.y) < EPS
+
 
 
 @dataclass
