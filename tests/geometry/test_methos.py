c33be8048306a54f773aabfcbbb0ff49e943fd14
JojiKoike <joji.koike@gmail.com>
2019-02-14 23:56:36 +0900

    Parallel Judge

--- tests/geometry/test_methos.py
+++ tests/geometry/test_methos.py
@@ -1,11 +1,11 @@
 import unittest
 
 from geometry.structs import Point, Vector, Segment
-from geometry.methods import is_orthogonal
+from geometry.methods import is_orthogonal, is_parallel
 
 
 class GeometryMethodTestCase(unittest.TestCase):
-    def test_is_orthogonal_vector_normal(self):
+    def test_is_orthogonal_normal(self):
         # Vector
         v_1: Vector = Vector(1.0, 1.0)
         v_2: Vector = Vector(1.0, -1.0)
@@ -20,6 +20,22 @@ class GeometryMethodTestCase(unittest.TestCase):
         s_1: Segment = Segment(p_1, p_2)
         s_2: Segment = Segment(p_3, p_4)
         self.assertEqual(is_orthogonal(s_1, s_2), True)
+
+    def test_is_parallel_normal(self):
+        # Vector
+        v_1: Vector = Vector(1.0, 1.0)
+        v_2: Vector = Vector(2.0, 2.0)
+        self.assertEqual(is_parallel(v_1, v_2), True)
+        # Points
+        p_1: Point = Point(1.0, 1.0)
+        p_2: Point = Point(2.0, 2.0)
+        p_3: Point = Point(-1.0, -1.0)
+        p_4: Point = Point(-2.0, -2.0)
+        self.assertEqual(is_parallel(p_1, p_2, p_3, p_4), True)
+        # Segments
+        s_1: Segment = Segment(p_1, p_2)
+        s_2: Segment = Segment(p_3, p_4)
+        self.assertEqual(is_parallel(s_1, s_2), True)
 
 
 if __name__ == '__main__':
