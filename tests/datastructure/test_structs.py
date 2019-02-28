c70fe158111460d062ec63b1f9608f3e8fb8db2c
JojiKoike <joji.koike@gmail.com>
2019-02-28 22:14:45 +0900

    Sort Point List by x or y Test

--- tests/datastructure/test_structs.py
+++ tests/datastructure/test_structs.py
@@ -34,6 +34,15 @@ class DataStructureStrutsTestCase(unittest.TestCase):
             self.assertEqual(points[i].y, i + 2)
             print(points[i])
 
+    def test_point_sort_by_x_normal(self):
+        points: List[Point] = [Point(1, 3, 2), Point(0, 1, 3), Point(2, 2, 1)]
+        sorted_x_points: List[Point] = sorted(points, key=lambda point: point.x)
+        self.assertEqual(1, sorted_x_points[0].x)
+        self.assertEqual(3, sorted_x_points[2].x)
+        sorted_y_points: List[Point] = sorted(points, key=lambda point: point.y)
+        self.assertEqual(1, sorted_y_points[0].y)
+        self.assertEqual(3, sorted_y_points[2].y)
+
 
 if __name__ == '__main__':
     unittest.main()
