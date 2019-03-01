69b78cd7180f3a5339902afa4c5439f10fb01634
JojiKoike <joji.koike@gmail.com>
2019-03-02 06:36:02 +0900

    Mod. Typo

--- tests/datastructure/test_structs.py
+++ tests/datastructure/test_structs.py
@@ -6,7 +6,7 @@ from typing import List
 from datastructure.structs import DisjointSet, Point
 
 
-class DataStructureStrutsTestCase(unittest.TestCase):
+class DataStructureStructsTestCase(unittest.TestCase):
     """
     DataStructure Structs Test Case Class
     """
