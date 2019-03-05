98f46949c1ef29ed25105f242dee8484a480406f
JojiKoike <joji.koike@gmail.com>
2019-03-05 12:45:01 +0900

    Prime Number Judgement and it's normal case test

--- number/methods.py
+++ number/methods.py
@@ -19,3 +19,23 @@ def power(x: int, n: int, m: int) -> int:
         else:
             res = (((res * res) % m) * x) % m
     return res
+
+
+def is_prime(n: int) -> bool:
+    """
+    Prime number judge method
+    :param n: judge target number
+    :return: Judge Results
+    """
+    if n <= 1:
+        # 0, 1 is not prime number
+        return False
+    if n % 2 == 0:
+        # even number is not prime number
+        return False
+    i: int = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
--- tests/number/test_methods.py
+++ tests/number/test_methods.py
@@ -1,11 +1,22 @@
 import unittest
-from number.methods import power
+from number.methods import power, is_prime
 
 
 class NumberMethodTestCase(unittest.TestCase):
 
     def test_power_normal(self):
         self.assertEqual(power(5, 8, 1000000007), 390625)
+
+    def test_is_prime_normal(self):
+        # 0, 1
+        self.assertEqual(False, is_prime(0))
+        self.assertEqual(False, is_prime(1))
+        # even
+        self.assertEqual(False, is_prime(2))
+        # not prime
+        self.assertEqual(False, is_prime(9))
+        # prime
+        self.assertEqual(True, is_prime(3))
 
 
 if __name__ == '__main__':
